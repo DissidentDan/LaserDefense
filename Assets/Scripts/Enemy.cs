f17e0396e8b50a2c2e7fe24f3c95053037ca11a9
Ryan Barker <rbarker22@hotmail.com>
2014-04-27 15:22:11 -0700

    Test Level with base NPC pathing

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : InteractsWithLaser {
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public override LaserReaction OnLaserHit(Laser laser, bool bPreview) {
+		return new LaserReaction(ELaserResponse.Stop, 0);
+	}
+	public void HandlePathComplete()
+	{
+		Debug.Log("Enemy has completed path");
+		//lose a life
+	}
+}
